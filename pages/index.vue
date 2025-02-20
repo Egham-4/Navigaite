6e2a65bac0414623120336c4a3fae70892438b56
Hamza Rafi <rafi.hamza2020@gmail.com>
2025-02-20 03:34:50 +0000

    Authentication

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+const { auth } = useSupabaseClient()
+const user = useSupabaseUser()
+
+const logout = async () => {
+  await auth.signOut()
+  navigateTo('/login')
+}
+
+console.log(user.value.user_metadata.firstname)
+console.log(user.value.user_metadata.lastname)
+const userFirstName = user.value.user_metadata.firstname
+const userLastName = user.value.user_metadata.lastname
+
+</script>
+<template>
+  <h1>Welcome {{ userFirstName }} {{ userLastName }}</h1>
+  <UButton @click='logout' label="signout" />
+</template>
